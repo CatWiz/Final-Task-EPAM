2ce1b38c7d556ddf69c353dade00aec8c0ae7247
CatWizard <thecatwizard124@gmail.com>
2025-06-20 09:24:08 +0300

    added test parametrization

--- Final Task/Factories/WebDriverFactory.cs
+++ Final Task/Factories/WebDriverFactory.cs
@@ -17,12 +17,13 @@ public static class WebDriverFactory
     private static readonly ThreadLocal<IWebDriver> LocalDriver = new();
     private static readonly Uri RemoteWebDriverUrl = new(TestsConfig.SeleniumGridUrl);
 
-    public static IWebDriver GetDriver(WebDriverType type)
+    public static IWebDriver GetDriver(WebDriverType type, Func<DriverOptions>? optionsFactory = null)
     {
         if (LocalDriver.Value is not null)
         {
             return LocalDriver.Value;
         }
+
         DriverOptions options = type switch
         {
             WebDriverType.Chrome => new ChromeOptions(),
@@ -30,11 +31,15 @@ public static class WebDriverFactory
             _ => throw new ArgumentOutOfRangeException(nameof(type))
         };
 
+        if (optionsFactory is not null)
+        {
+            options = optionsFactory.Invoke();
+        }
+
         var remoteDriver = new RemoteWebDriver(RemoteWebDriverUrl, options.ToCapabilities());
         remoteDriver.Manage().Window.Maximize();
 
         LocalDriver.Value = remoteDriver;
-
         return LocalDriver.Value;
     }
 }
---
+++ Final Task/Tests/LoginPageTests.cs
@@ -0,0 +1,106 @@
+using FinalTask.Config;
+using FinalTask.Factories;
+using FinalTask.PageObjects;
+using FluentAssertions;
+using OpenQA.Selenium;
+
+namespace FinalTask.Tests;
+
+[TestClass]
+public class LoginPageTests : IDisposable
+{
+    private IWebDriver? _driver;
+
+    public static IEnumerable<(WebDriverType, Func<DriverOptions>?)> TestCases
+    {
+        get
+        {
+            yield return (WebDriverType.Chrome, null);
+            yield return (WebDriverType.Firefox, null);
+        }
+    }
+
+    private void InitializeDriver(WebDriverType type, Func<DriverOptions>? optionsFactory)
+    {
+        this._driver = WebDriverFactory.GetDriver(type, optionsFactory);
+        this._driver.Navigate().GoToUrl(TestsConfig.BaseUrl);
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        this._driver?.Quit();
+        this._driver?.Dispose();
+        this._driver = null;
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(TestCases), DynamicDataSourceType.Property)]
+    public void LoginAfterClearingCredentialsShouldShowUsernameMissingError(WebDriverType type, Func<DriverOptions>? optionsFactory)
+    {
+        this.InitializeDriver(type, optionsFactory);
+
+        var loginPage = new LoginPageObject(this._driver ?? throw new InvalidOperationException("Driver not initialized"));
+
+        loginPage.EnterUsername("asdf");
+        loginPage.EnterPassword("zxcv");
+
+        loginPage.ClearUsername();
+        loginPage.ClearPassword();
+
+        loginPage.LoginExpectFailure();
+
+        _ = this._driver.Url.TrimEnd('/')
+            .Should().Be(TestsConfig.BaseUrl, "login with no credentials should stay on the login page");
+
+        var errorMessage = loginPage.GetErrorMessage();
+        _ = errorMessage
+            .Should().Be("Epic sadface: Username is required", $"error message should indicate missing username, username: {loginPage.GetUsername()}, password: {loginPage.GetPassword()}");
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(TestCases), DynamicDataSourceType.Property)]
+    public void LoginAfterClearingPasswordShouldShowPasswordMissingError(WebDriverType type, Func<DriverOptions>? optionsFactory)
+    {
+        this.InitializeDriver(type, optionsFactory);
+
+        var loginPage = new LoginPageObject(this._driver ?? throw new InvalidOperationException("Driver not initialized"));
+
+        loginPage.EnterUsername("asdf");
+        loginPage.EnterPassword("zxcv");
+
+        loginPage.ClearPassword();
+
+        loginPage.LoginExpectFailure();
+
+        _ = this._driver.Url.TrimEnd('/')
+            .Should().Be(TestsConfig.BaseUrl, "login with no credentials should stay on the login page");
+
+        var errorMessage = loginPage.GetErrorMessage();
+        _ = errorMessage
+            .Should().Be("Epic sadface: Password is required", $"error message should indicate missing password, username: {loginPage.GetUsername()}, password: {loginPage.GetPassword()}");
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(TestCases), DynamicDataSourceType.Property)]
+    public void LoginWithValidCredentialsShouldRedirectToInventoryPage(WebDriverType type, Func<DriverOptions>? optionsFactory)
+    {
+        this.InitializeDriver(type, optionsFactory);
+
+        var loginPage = new LoginPageObject(this._driver ?? throw new InvalidOperationException("Driver not initialized"));
+
+        loginPage.EnterUsername("standard_user");
+        loginPage.EnterPassword("secret_sauce");
+
+        _ = loginPage.LoginExpectSuccess();
+
+        _ = this._driver.Url
+            .Should().Be($"{TestsConfig.BaseUrl}/inventory.html", "login with valid credentials should redirect to the inventory page");
+    }
+
+    public void Dispose()
+    {
+        this._driver?.Quit();
+        this._driver?.Dispose();
+    }
+}
