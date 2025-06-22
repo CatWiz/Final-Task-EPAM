e1d2b2b0bbcb4e2d670d240307941dc2eb343302
CatWizard <thecatwizard124@gmail.com>
2025-06-22 09:12:38 +0300

    added documentation for BrowserOptions

--- Final Task/Factories/BrowserOptions.cs
+++ Final Task/Factories/BrowserOptions.cs
@@ -5,8 +5,27 @@ namespace FinalTask.Factories;
 public class BrowserOptions
 {
     public required DriverOptions DriverOptions { get; init; }
+
+    /// <summary>
+    /// Indicates whether the browser window should be maximized.
+    /// </summary>
     public required bool Maximize { get; init; }
+
+    /// <summary>
+    /// String representation of command line arguments for the browser.
+    /// Used for logging.
+    /// </summary>
     public required string Arguments { get; init; }
+
+    /// <summary>
+    /// String representation of browser preferences.
+    /// Used for logging.
+    /// </summary>
     public required string Preferences { get; init; }
+
+    /// <summary>
+    /// String representation of browser capabilities.
+    /// Used for logging.
+    /// </summary>
     public required string Capabilities { get; init; }
 }
